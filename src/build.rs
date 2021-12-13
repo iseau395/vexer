1770da527e91778bcbb422ccebcf29e54a00e5d8
iseau395 <70857830+iseau395@users.noreply.github.com>
2021-12-12 18:35:12 -0800

    Made the path flag actually work and more
    * removed debug code
    * formatted code better
    * made build output live
    * simplified some code
    * linux might work?

--- src/build.rs
+++ src/build.rs
@@ -2,7 +2,7 @@ use std::path;
 use std::process;
 
 pub const VEXCODE: &str = "C:/Program Files (x86)/Vex Robotics/VEXcode Pro V5";
-pub const TOOLCHAIN: &str= "/toolchain/vexv5/win32";
+pub const TOOLCHAIN: &str = "/toolchain/vexv5/win32";
 
 pub fn build(file: &std::path::PathBuf) -> std::result::Result<(), ()> {
     let vexcode = path::PathBuf::from(VEXCODE);
@@ -12,40 +12,43 @@ pub fn build(file: &std::path::PathBuf) -> std::result::Result<(), ()> {
         Err(())
     } else {
         let toolchain = [VEXCODE, TOOLCHAIN].concat();
-        let make = [ &toolchain, "/tools/bin/make.exe"].concat();
-        let sdk = [ &VEXCODE, "/sdk" ].concat();
+        let make = [&toolchain, "/tools/bin/make.exe"].concat();
+        let sdk = [&VEXCODE, "/sdk"].concat();
 
-        let args = [
-            &["T=", &sdk.as_str()].concat(),
-            "V=1"
-        ];
+        let args = [&["T=", &sdk.as_str()].concat(), "V=0"];
 
-        println!("\"{}\" {}", make.as_str(), args.join(" "));
-        
-        let output = if cfg!(target_os = "windows") {
-            process::Command::new(make.as_str())
-                    .args(args)
-                    .current_dir(std::fs::canonicalize(file).expect("Failed to turn file into path!"))
-                    .env("path", [
-                        std::env::var("path").expect("Failed to fetch PATH env variable!"),
-                        [ toolchain.as_str(), "/clang/bin" ].concat(),
-                        [ toolchain.as_str(), "/gcc/bin" ].concat(),
-                        [ toolchain.as_str(), "/tools/bin" ].concat()
-                    ].join(";"))
-                    .output()
-                    .expect("failed to execute process")
+        let path = if cfg!(target_os = "windows") {
+            "path"
         } else {
-            process::Command::new("sh")
-                    .arg("-c")
-                    .arg("echo hello")
-                    .output()
-                    .expect("failed to execute process")
+            "PATH"
         };
 
-        use std::io::Write;
+        std::fs::remove_dir_all(file.join("build")).expect("Failed to delete old build!");
 
-        std::io::stdout().write_all(&output.stdout).unwrap();
-        std::io::stderr().write_all(&output.stderr).unwrap();
+        let mut child = 
+            process::Command::new(make.as_str())
+                .args(args)
+                .current_dir(file)
+                .env(
+                    path,
+                    [
+                        std::env::var(path).expect("Failed to fetch PATH env variable!"),
+                        [toolchain.as_str(), "/clang/bin"].concat(),
+                        [toolchain.as_str(), "/gcc/bin"].concat(),
+                        [toolchain.as_str(), "/tools/bin"].concat(),
+                    ]
+                    .join(";"),
+                )
+                .spawn()
+                .expect("failed to execute process");
+
+        let ecode = child.wait()
+            .expect("failed to wait on child");
+
+        if !ecode.success() {
+            println!("Build Failed!");
+            return Err(())
+        }
 
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,20 @@ use structopt::StructOpt;
 
 mod build;
 
-
 /// A structure for the arguments
-#[derive(StructOpt)]
-#[derive(std::fmt::Debug)]
-struct Cli {
+#[derive(std::fmt::Debug, StructOpt)]
+#[structopt(about = "Build VEXCode V5 Pro projects and download them to the V5 brain")]
+struct Vexer {
     /// The command to run
     command: String,
 
     /// The path to the input file
-    #[structopt(parse(from_os_str))]
+    #[structopt(short, long, default_value = ".", parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
-#[allow(unused_variables)]
-
 fn main() {
-    let args = Cli::from_args();
+    let args = Vexer::from_args();
 
     if args.command == String::from("build") {
         build::build(&args.path).expect("Build Failed");
